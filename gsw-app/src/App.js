db7ea375b389f387302142f24f6aaa3d3e301296
Rodrigo Augusto Silva Lima <rodrigoaslima@hotmail.com>
2019-09-30 16:35:01 -0300

    add/ Adicionado navBar

---
+++ gsw-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import NavBar from './ui/NavBar'
+//import './css/pure-min.css';
+//import './css/side-menu.css';
+
+function App() {
+  return (
+
+    <div className="container">
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
